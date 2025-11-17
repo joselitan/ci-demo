0956f7805bdf48bd5e988965a8baedd11242b438
Jose Martin <jose.martin@allente.tv>
2025-11-17 11:11:04 +0100

    adding calculatorTwo application and tests

---
+++ src/test/java/org/example/CalculatorTwoTest.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class CalculatorTwoTest {
+
+    @Test
+    void testAdd(){
+        CalculatorTwo calc = new CalculatorTwo();
+        assertEquals(5, calc.calculate("add", 2, 3));
+    }
+
+    @Test
+    void testSubstract(){
+        CalculatorTwo calc = new CalculatorTwo();
+        assertEquals(5, calc.calculate("subtract", 10, 5));
+    }
+
+    @Test
+    void testMultiply(){
+        CalculatorTwo calc = new CalculatorTwo();
+        assertEquals(10, calc.calculate("multiply", 5, 2));
+    }
+
+    @Test
+    void testDivision(){
+        CalculatorTwo calc = new CalculatorTwo();
+        assertEquals(5, calc.calculate("divide", 10, 2));
+    }
+}
