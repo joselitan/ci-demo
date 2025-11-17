201a209f2f2a7b80ab83c04bfbb2ec69c811ba12
Jose Martin <jose.martin@allente.tv>
2025-11-17 12:02:08 +0100

    corrected the class name to CalculatorTwoRefactoredTest

--- src/test/java/org/example/CalculatorTwoRefactoredTest.java
+++ src/test/java/org/example/CalculatorTwoRefactoredTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-class CalculatorTwoTestRefacatored {
+class CalculatorTwoRefactoredTest {
 
     @Test
     void testAdd(){
